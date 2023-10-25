13c60e931c143192ff0a3d91378281b8f9beb23a
Jesus Carpintero <jesus.carpintero@adevinta.com>
2023-10-25 17:54:41 +0200

    Revert "Use add-and-commit"
    This reverts commit 4bf7128004865e21b89ab3fd1f0c8c1fa6e45f95.

--- patch.sh
+++ patch.sh
@@ -5,3 +5,15 @@ set -o pipefail
 
 curl -s "https://raw.githubusercontent.com/adevinta/vulcan-api/$SRC_BRANCH/swagger/swagger.json" |
     jq --arg host "$API_HOST" '.host = $host' > docs/swagger.json
+
+git add docs/swagger.json
+
+git config --global user.name "github"
+git config --global user.email "github@example.org"
+
+if ! git diff-index --quiet --cached HEAD; then
+    git commit -m "Update docs"
+    git push
+else
+    echo "No updates need to be commited"
+fi
