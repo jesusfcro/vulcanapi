4bf7128004865e21b89ab3fd1f0c8c1fa6e45f95
Jesus Carpintero <jesus.carpintero@adevinta.com>
2023-10-25 17:47:29 +0200

    Use add-and-commit

--- patch.sh
+++ patch.sh
@@ -5,11 +5,3 @@ set -o pipefail
 
 curl -s "https://raw.githubusercontent.com/adevinta/vulcan-api/$SRC_BRANCH/swagger/swagger.json" |
     jq --arg host "$API_HOST" '.host = $host' > docs/swagger.json
-
-git add docs/swagger.json
-if ! git diff-index --quiet --cached HEAD; then
-    git commit -m "Update docs"
-    git push
-else
-    echo "No updates need to be commited"
-fi
